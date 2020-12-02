1e5694f1473a4fd28f75f0d537cd6247bb5f2c96
Andre985 <55833240+Andre985@users.noreply.github.com>
2020-12-02 19:07:25 +0100

    .net core rettelse.

--- Microwave.Test.Integration/Step2.cs
+++ Microwave.Test.Integration/Step2.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     public class Step2
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
     }
 }
